33df7abb0bb4aee32264ba2c152d60f8f4caea9e
BrushankPatra <brushankpatra@gmail.com>
2024-05-17 18:06:13 +0530

    score incrementing functionality added to buttons

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+
+let p1Score = 0, p2Score = 0;
 
 p1Button.addEventListener("click", event => {
-    alert("P1 Button Clicked");
+    p1Score++;
+    p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", event => {
-    alert("P2 Button Clicked");
+    p2Score++;
+    p2Display.textContent = p2Score;
 });
