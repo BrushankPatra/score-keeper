fd842fb51e1a3dcf63b928471b870cbc2c86d4d8
BrushankPatra <brushankpatra@gmail.com>
2024-05-17 18:16:48 +0530

    functions of buttons limited according to game is over or not

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
 let p1Score = 0, p2Score = 0, winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener("click", event => {
-    if (p1Score < winningScore) {
+    if (!isGameOver) {
         p1Score++;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
 });
 
 p2Button.addEventListener("click", event => {
-    if (p2Score < winningScore) {
+    if (!isGameOver) {
         p2Score++;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
 });
