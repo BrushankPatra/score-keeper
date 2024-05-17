597eda164ccd9c4a85906351e671699fd66d9a42
BrushankPatra <brushankpatra@gmail.com>
2024-05-17 18:01:02 +0530

    event listeners added to score buttons

---
+++ app.js
@@ -0,0 +1,10 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+
+p1Button.addEventListener("click", event => {
+    alert("P1 Button Clicked");
+});
+
+p2Button.addEventListener("click", event => {
+    alert("P2 Button Clicked");
+});
