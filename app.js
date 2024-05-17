24144b11a06beaedeb59eeb2231ff92371ed1ea6
BrushankPatra <brushankpatra@gmail.com>
2024-05-17 18:11:06 +0530

    limited the scores to a winning score

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
-let p1Score = 0, p2Score = 0;
+let p1Score = 0, p2Score = 0, winningScore = 5;
 
 p1Button.addEventListener("click", event => {
-    p1Score++;
-    p1Display.textContent = p1Score;
+    if (p1Score < winningScore) {
+        p1Score++;
+        p1Display.textContent = p1Score;
+    }
 });
 
 p2Button.addEventListener("click", event => {
-    p2Score++;
-    p2Display.textContent = p2Score;
+    if (p2Score < winningScore) {
+        p2Score++;
+        p2Display.textContent = p2Score;
+    }
 });
