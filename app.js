0d75cde01d420eeb757888b06d680a3e7214b069
BrushankPatra <brushankpatra@gmail.com>
2024-05-17 20:34:15 +0530

    event listener added for select menu and some corrections

--- app.js
+++ app.js
@@ -3,34 +3,44 @@ const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playTo");
 
-let p1Score = 0, p2Score = 0, winningScore = 5;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
 let isGameOver = false;
 
-p1Button.addEventListener("click", event => {
+p1Button.addEventListener("click", function () {
     if (!isGameOver) {
-        p1Score++;
+        p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
         }
         p1Display.textContent = p1Score;
     }
-});
+})
 
-p2Button.addEventListener("click", event => {
+p2Button.addEventListener("click", function () {
     if (!isGameOver) {
-        p2Score++;
+        p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
         }
         p2Display.textContent = p2Score;
     }
-});
+})
 
-resetButton.addEventListener("click", event => {
+winningScoreSelect.addEventListener("change", function () {
+    winningScore = parseInt(this.value);
+    resetAll();
+})
+
+resetButton.addEventListener("click", resetAll);
+
+function resetAll() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
-})
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}
